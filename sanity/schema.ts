57103df5537c2f2c1e82679cd1407ba15111e4ce
Dionne Chelini <76867156+DionneChelini@users.noreply.github.com>
2023-12-04 22:49:08 +1100

    fix: removed sanity to get deploy working

--- sanity/schema.ts
+++
@@ -1,8 +0,0 @@
-import { type SchemaTypeDefinition } from "sanity";
-import { english } from "./schemas/english";
-import { chinese } from "./schemas/chinese";
-import courseStructure from "./schemas/courseStructure";
-import { exercise } from "./schemas/interactiveExercise";
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [english, chinese, courseStructure, exercise],
-};
