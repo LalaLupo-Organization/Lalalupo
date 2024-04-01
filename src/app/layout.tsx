94f76b4a9e7c33d86dac493126d1439929287d9a
Dionne Chelini <dionne@dotapparel.com.au>
2024-04-01 14:22:51 +1100

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import FullStoryScript from "@/scripts/fullstory";
 import { ThemeProvider } from "@/providers/themeProvider";
 // These styles apply to every route in the application
 import "./globals.css";
+import { Suspense } from "react";
 import Navbar from "@/components/Navbars/Navbar";
 import { ClerkProvider } from "@clerk/nextjs";
 import Footer from "@/components/Footers/Footer";
@@ -14,8 +15,7 @@ export default function RootLayout({ children }: { children: any }) {
           <body>
             <Navbar />
             <FullStoryScript />
-
-            {children}
+            <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
             <Footer />
           </body>
         </ThemeProvider>
